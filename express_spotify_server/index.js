cc506cb97591b436c798298bd4dc27a6542ebb7a
Abishek thiru <101636164+abishek1405@users.noreply.github.com>
2026-03-15 08:30:33 +0000

    update

--- express_spotify_server/index.js
+++ express_spotify_server/index.js
@@ -14,13 +14,14 @@ app.use("/assets", express.static(path.join(__dirname, "assets")));
 
 
 
-// MongoDB Atlas Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log("✅ MongoDB Connected");
+  app.listen(process.env.PORT || 2000, () => {
+    console.log(`🚀 Server running on ${process.env.PORT || 2000}`);
+  });
 })
-.then(() => console.log("✅ MongoDB Atlas Connected"))
-.catch(err => console.error("❌ MongoDB Error:", err));
+.catch(err => console.log(err));
 
 
 const userSchema = new mongoose.Schema({
@@ -71,7 +72,8 @@ app.post("/login", async (req, res) => {
       return res.status(400).json({ message: "Invalid Password" });
     }
 
-    const jwtToken = jwt.sign({ username }, "key");
+    const jwtToken = jwt.sign({ username }, process.env.JWT_SECRET);
+
     res.json({ jwt_token: jwtToken });
   } catch (e) {
     console.error(e.message);
@@ -79,8 +81,10 @@ app.post("/login", async (req, res) => {
   }
 });
 
-app.listen(2000, () => {
-  console.log("🚀 Server running at http://localhost:2000/");
+const PORT = process.env.PORT || 2000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
 
 
@@ -98,7 +102,6 @@ const Song = mongoose.model("Song", songSchema);
 
 
 
-
 app.get("/getsongs", async (req, res) => {
   try {
     const allSongs = await Song.find();
@@ -112,12 +115,16 @@ app.get("/getsongs", async (req, res) => {
 
 app.get("/getsongs/:id", async(req, res) => {
   const songId = parseInt(req.params.id);
-  const allSongs = await Song.find();
-  const song = allSongs.find((s) => s.id === songId);
+  // const allSongs = await Song.find();
+  // const song = allSongs.find((s) => s.id === songId);
 
-  if (song) {
-    res.json(song);
-  } else {
-    res.status(404).json({ message: "Song not found" });
-  }
+  // if (song) {
+  //   res.json(song);
+  // } else {
+  //   res.status(404).json({ message: "Song not found" });
+  // }
+
+  const song = await Song.findOne({ id: songId });
+  if (song) res.json(song);
+  else res.status(404).json({ message: "Song not found" });
 });
